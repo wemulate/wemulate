17ab83b06620f982b0f1bde9c34b5dc3af01b83f
Julian Klaiber <julian@klaiber.me>
2021-04-27 07:58:32 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-
 from setuptools import setup, find_packages
 from wemulate.core.version import get_version
 
 VERSION = get_version()
 
-f = open('README.md', 'r')
+f = open("README.md", "r")
 LONG_DESCRIPTION = f.read()
 f.close()
 
 setup(
-    name='wemulate',
+    name="wemulate",
     version=VERSION,
-    description='A modern WAN Emulator',
+    description="A modern WAN Emulator",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    author='Julian Klaiber & Severin Dellsperger',
-    author_email='ins.support@ost.ch',
-    url='https://github.com/wemulate/wemulate/',
-    license='GPL-3.0',
-    packages=find_packages(exclude=['ez_setup', 'tests*']),
-    package_data={'wemulate': ['templates/*']},
+    long_description_content_type="text/markdown",
+    author="Julian Klaiber, Severin Dellsperger",
+    author_email="julian.klaiber@ost.ch, severin.dellsperger@ost.ch",
+    url="https://github.com/wemulate/wemulate/",
+    license="GPL-3.0",
+    packages=find_packages(exclude=["ez_setup", "tests*"]),
+    package_data={"wemulate": ["templates/*"]},
     include_package_data=True,
     entry_points="""
         [console_scripts]
