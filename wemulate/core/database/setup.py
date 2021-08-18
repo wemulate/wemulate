023a021c508971d4e7d4c8a9c3323533a54fbf68
Severin Dellsperger <severin.dellsperger@gmail.com>
2021-08-18 17:14:17 +0200

    add session parameter to db setup

--- wemulate/core/database/setup.py
+++ wemulate/core/database/setup.py
@@ -1,4 +1,5 @@
 import socket, string
+from sqlalchemy.orm.session import Session
 from wemulate.utils.settings import get_interfaces, get_mgmt_interfaces
 from wemulate.core.database.models import (
     ProfileModel,
@@ -9,8 +10,7 @@ from wemulate.core.database.models import (
 from wemulate.core.database.decorators import use_db_session
 
 
-@use_db_session
-def _pre_setup_profile():
+def _pre_setup_profile(session: Session) -> None:
     DEFAULT_PROFILE_NAME: str = "default"
     if (
         not session.query(ProfileModel)
@@ -20,16 +20,14 @@ def _pre_setup_profile():
         session.add(ProfileModel(DEFAULT_PROFILE_NAME))
 
 
-@use_db_session
-def _pre_setup_device():
+def _pre_setup_device(session: Session) -> None:
     hostname: str = socket.gethostname()
     PROFILE_ID: int = 1
     if not session.query(DeviceModel).filter_by(device_name=hostname).first():
         session.add(DeviceModel(hostname, PROFILE_ID, get_mgmt_interfaces()[0]))
 
 
-@use_db_session
-def _pre_setup_logical_interfaces(ascii_index):
+def _pre_setup_logical_interfaces(session: Session, ascii_index: int) -> None:
     if (
         not session.query(LogicalInterfaceModel)
         .filter_by(logical_name="LAN-" + string.ascii_uppercase[ascii_index])
@@ -38,8 +36,7 @@ def _pre_setup_logical_interfaces(ascii_index):
         session.add(LogicalInterfaceModel("LAN-" + string.ascii_uppercase[ascii_index]))
 
 
-@use_db_session
-def _pre_setup_interfaces() -> None:
+def _pre_setup_interfaces(session: Session) -> None:
     count: int = 0
     DEVICE_ID: int = 1
     for physical_interface in get_interfaces():
@@ -48,12 +45,13 @@ def _pre_setup_interfaces() -> None:
             .filter_by(physical_name=physical_interface)
             .first()
         ):
-            _pre_setup_logical_interfaces(count)
+            _pre_setup_logical_interfaces(session, count)
             session.add(InterfaceModel(physical_interface, DEVICE_ID, count + 1))
             count += 1
 
 
-def pre_setup_database() -> None:
-    _pre_setup_profile()
-    _pre_setup_device()
-    _pre_setup_interfaces()
+@use_db_session
+def pre_setup_database(session: Session) -> None:
+    _pre_setup_profile(session)
+    _pre_setup_device(session)
+    _pre_setup_interfaces(session)
