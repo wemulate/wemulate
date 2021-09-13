3962ab2ce3543fda511efa0796599014f16aa413
Severin <severin.dellsperger@gmail.com>
2021-09-13 14:35:58 +0000

    change loader to relative path

--- wemulate/utils/rendering.py
+++ wemulate/utils/rendering.py
@@ -1,11 +1,14 @@
 from typing import Dict, Text
 from jinja2 import Environment, FileSystemLoader
 from jinja2.environment import Template
+import os
 
 
 def rendering(data: Dict[str, int], template_file_name: str) -> Text:
     env: Environment = Environment(
-        loader=FileSystemLoader("./wemulate/templates"),
+        loader=FileSystemLoader(
+            os.path.join(os.path.dirname(__file__), "../templates")
+        ),
         trim_blocks=True,
         lstrip_blocks=True,
     )
