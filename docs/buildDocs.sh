13e3c7c016f644e093b4ed3a07d58aa1c2292db6
Julian Klaiber <julian@klaiber.me>
2021-09-14 16:27:13 +0200

    Fix requirement

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -6,7 +6,7 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-pip python3-sphinx python3-sphinx-rtd-theme
+apt-get -y install git rsync python3-pip python3-sphinx python3-sphinx-rtd-theme python3-netifaces
 pip install requirements.txt
  
 #####################
