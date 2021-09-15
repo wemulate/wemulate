6b7501517009b6f9832c4e84a9549f21a899907d
Julian Klaiber <julian@klaiber.me>
2021-09-15 10:19:50 +0200

    Debug

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -29,44 +29,44 @@ make -C docs html
 # Update GitHub Pages #
 #######################
  
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+# git config --global user.name "${GITHUB_ACTOR}"
+# git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
  
-docroot=`mktemp -d`
-rsync -av "docs/_build/html/" "${docroot}/"
+# docroot=`mktemp -d`
+# rsync -av "docs/_build/html/" "${docroot}/"
  
-pushd "${docroot}"
+# pushd "${docroot}"
  
-# don't bother maintaining history; just generate fresh
-git init
-git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git checkout -b gh-pages
+# # don't bother maintaining history; just generate fresh
+# git init
+# git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+# git checkout -b gh-pages
  
-# add .nojekyll to the root so that github won't 404 on content added to dirs
-# that start with an underscore (_), such as our "_content" dir..
-touch .nojekyll
+# # add .nojekyll to the root so that github won't 404 on content added to dirs
+# # that start with an underscore (_), such as our "_content" dir..
+# touch .nojekyll
  
-# Add README
-cat > README.md <<EOF
-# GitHub Pages Cache
+# # Add README
+# cat > README.md <<EOF
+# # GitHub Pages Cache
  
-Nothing to see here. The contents of this branch are essentially a cache that's not intended to be viewed on github.com.
+# Nothing to see here. The contents of this branch are essentially a cache that's not intended to be viewed on github.com.
  
  
-If you're looking to update our documentation, check the relevant development branch's 'docs/' dir.
-EOF
+# If you're looking to update our documentation, check the relevant development branch's 'docs/' dir.
+# EOF
  
-# copy the resulting html pages built from sphinx above to our new git repo
-git add .
+# # copy the resulting html pages built from sphinx above to our new git repo
+# git add .
  
-# commit all the new files
-msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF} by ${GITHUB_ACTOR}"
-git commit -am "${msg}"
+# # commit all the new files
+# msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF} by ${GITHUB_ACTOR}"
+# git commit -am "${msg}"
  
-# overwrite the contents of the gh-pages branch on our github.com repo
-git push deploy gh-pages --force
+# # overwrite the contents of the gh-pages branch on our github.com repo
+# git push deploy gh-pages --force
  
-popd # return to main repo sandbox root
+# popd # return to main repo sandbox root
  
-# exit cleanly
-exit 0
+# # exit cleanly
+# exit 0
